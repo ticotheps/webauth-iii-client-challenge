00284e870d340368884855550be0fd99fd31a807
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-04 12:50:44 -0400

    Adds a 'state' object on the <Login /> and adds attributes to each <input> element in the form

--- client-side-auth-iii/src/login/Login.js
+++ client-side-auth-iii/src/login/Login.js
@@ -2,23 +2,38 @@ import React from "react";
 import axios from "axios";
 
 class Login extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2>Login</h2>
-        <form action="">
-          <div>
-            <label htmlFor="" />
-            <input type="text" />
-          </div>
-          <div>
-            <label htmlFor="" />
-            <input type="text" />
-          </div>
-        </form>
-      </div>
-    );
-  }
+    state = {
+        username: 'tico',
+        password: 'blue'
+    }
+
+    render() {
+        return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit="this.handleSubmit">
+            <div>
+                <label htmlFor="username" />
+                <input
+                id="username"
+                value="this.state.username"
+                onChange="this.handleInputChange"
+                type="text"
+                />
+            </div>
+            <div>
+                <label htmlFor="password" />
+                <input
+                id="password"
+                value="this.state.password"
+                onChange="this.handleInputChange"
+                type="password"
+                />
+            </div>
+            </form>
+        </div>
+        );
+    }
 }
 
 export default Login;
