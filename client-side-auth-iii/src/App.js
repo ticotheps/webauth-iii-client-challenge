a71eca6a74bfaaf31125dc0ddcae2973904c084e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-04 13:19:48 -0400

    Imports 'Route, NavLink, withRouter' into App.js, as well as 'Login.js', to build Routes for our <Login />

--- client-side-auth-iii/src/App.js
+++ client-side-auth-iii/src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { Route, NavLink, withRouter } from 'react-router-dom';
+
 import './App.css';
+import Login from './login/Login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <header>
+          <nav>
+            <NavLink to="/login">Login</NavLink>
+            &nbsp;|&nbsp;
+            <NavLink to="/users">Users</NavLink>
+          </nav>
+        </header>
+        <main>
+          <Route path="/login" component={Login}></Route>
+          <Route path="/users" component={Users}></Route>
+        </main>
       </div>
     );
   }
