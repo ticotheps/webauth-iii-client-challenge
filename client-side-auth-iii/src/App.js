6a1bfe6c94606f44189f2034f67ed9eebde7a46b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-04 16:45:11 -0400

    Uses render props for all routes in App.js

--- client-side-auth-iii/src/App.js
+++ client-side-auth-iii/src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import { Route, NavLink, withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { Route, NavLink, withRouter } from "react-router-dom";
 
-import './App.css';
-import Home from './home/Home';
-import Login from './login/Login';
-import Users from './users/Users';
+import "./App.css";
+import Home from "./home/Home";
+import Login from "./login/Login";
+import Users from "./users/Users";
 
 class App extends Component {
   render() {
@@ -12,7 +12,9 @@ class App extends Component {
       <div className="App">
         <header>
           <nav>
-            <NavLink exact to="/">Home</NavLink>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
             &nbsp;|&nbsp;
             <NavLink to="/login">Login</NavLink>
             &nbsp;|&nbsp;
@@ -22,18 +24,37 @@ class App extends Component {
           </nav>
         </header>
         <main>
-          <Route exact path="/" component={Home}></Route>
-          <Route path="/login" component={Login}></Route>
-          <Route path="/users" component={Users}></Route>
+          <Route
+            exact
+            path="/"
+            render={props => {
+              console.log(props);
+              return <Home {...props} />;
+            }}
+          />
+          <Route
+            path="/login"
+            render={props => {
+              console.log(props);
+              return <Login {...props} />;
+            }}
+          />
+          <Route
+            path="/users"
+            render={props => {
+              console.log(props);
+              return <Users {...props} />;
+            }}
+          />
         </main>
       </div>
     );
   }
 
   logout = () => {
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
 
-    this.props.history.push('/login');
+    this.props.history.push("/login");
   };
 }
 
