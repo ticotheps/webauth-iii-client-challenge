38063d28b12728057a30848aa10bc5696e21ff8e
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-04 12:35:07 -0400

    Creates a new 'login' folder and nests a new 'Login.js' file inside that folder to build our a new <Login /> component

---
+++ client-side-auth-iii/src/login/Login.js
@@ -0,0 +1,24 @@
+import React from "react";
+import axios from "axios";
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Login</h2>
+        <form action="">
+          <div>
+            <label htmlFor="" />
+            <input type="text" />
+          </div>
+          <div>
+            <label htmlFor="" />
+            <input type="text" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
