be36df9f6766f7d1e8865458b13862a4350c46d1
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-04-04 13:05:04 -0400

    Builds out a handleSubmit() function inside of Login.js

--- client-side-auth-iii/src/login/Login.js
+++ client-side-auth-iii/src/login/Login.js
@@ -2,48 +2,59 @@ import React from "react";
 import axios from "axios";
 
 class Login extends React.Component {
-    state = {
-        username: 'tico',
-        password: 'blue'
-    }
+  state = {
+    username: "tico",
+    password: "blue"
+  };
 
-    render() {
-        return (
-        <div>
-            <h2>Login</h2>
-            <form onSubmit="this.handleSubmit">
-            <div>
-                <label htmlFor="username" />
-                <input
-                id="username"
-                value="this.state.username"
-                onChange="this.handleInputChange"
-                type="text"
-                />
-            </div>
-            <div>
-                <label htmlFor="password" />
-                <input
-                id="password"
-                value="this.state.password"
-                onChange="this.handleInputChange"
-                type="password"
-                />
-            </div>
-            </form>
-        </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h2>Login</h2>
+        <form onSubmit="this.handleSubmit">
+          <div>
+            <label htmlFor="username" />
+            <input
+              id="username"
+              value="this.state.username"
+              onChange="this.handleInputChange"
+              type="text"
+            />
+          </div>
+          <div>
+            <label htmlFor="password" />
+            <input
+              id="password"
+              value="this.state.password"
+              onChange="this.handleInputChange"
+              type="password"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 
-    handleInputChange = event => {
-        const { id, value } = event.target;
+  handleInputChange = event => {
+    const { id, value } = event.target;
 
-        this.setState = ({ [id]: value });
-    };    
+    this.setState = { [id]: value };
+  };
 
-    handleSubmit = event => {
+  handleSubmit = event => {
+    event.preventDefault();
 
-    }
-};
+    const endpoint = "http://localhost:4000/api/auth/login";
+
+    axios
+      .post(endpoint, this.state)
+      .then(res => {
+        localStorage.setItem("jwt", res.data.token);
+
+        this.props.history.push("/users");
+      })
+      .catch(error => console.error(error));
+  };
+}
 
 export default Login;
